56980ca83f02c321d8c3f05fab3aeb67aa83c91b
James Scott-Brown <james@jamesscottbrown.com>
2022-12-12 11:00:30 +0000

    don't throw error in detail view for activity with no files (#127)

--- src/components/ArtifactDetailWindow.tsx
+++ src/components/ArtifactDetailWindow.tsx
@@ -62,7 +62,8 @@ const ArtifactDetailWindow = (props: DetailProps) => {
 
   const [fragSelected, setFragSelected] = useState<boolean>(false);
 
-  let selectedFileType = selectedArtifact.activity.files[selectedArtifact.artifactIndex].title.split('.').at(-1);
+  const selectedFileType = (selectedArtifact && selectedArtifact.activity.files && selectedArtifact.activity.files.length > 0)
+    ? selectedArtifact.activity.files[selectedArtifact.artifactIndex].title.split('.').at(-1) : null;
 
   useEffect(() => {
     if (editable.length === projectData.entries.length - 1) {
@@ -121,7 +122,9 @@ const ArtifactDetailWindow = (props: DetailProps) => {
                 {
                   activity: selectActivity,
                   artifactUid:
-                    selectActivity && selectActivity.files[0].artifact_uid
+                    selectActivity &&
+                    selectActivity.files.length > 0 &&
+                    selectActivity.files[0].artifact_uid
                       ? selectActivity.files[0].artifact_uid
                       : null,
                   hopReason: 'time hop back',
@@ -190,7 +193,7 @@ const ArtifactDetailWindow = (props: DetailProps) => {
             paddingTop: 5,
           }}
         >{`Artifact: ${
-          (selectedArtifact && selectedArtifact.artifactIndex > -1)
+          (selectedArtifact && selectedArtifact.artifactIndex > -1 && selectedArtifact.activity.files && selectedArtifact.activity.files.length > 0)
             ? selectedArtifact.activity.files[selectedArtifact.artifactIndex]
                 .title
             : 'No artifacts with this activity'
